318a4a6309d6c1d1b3f3e1dc091c291fd78a3ff2
ravikiran7090 <ravikirannagure@gmail.com>
2022-02-01 13:34:03 +0530

    sdet file is pushing

---
+++ SDET29/src/test/java/demoGit/DemoGit.java
@@ -0,0 +1,15 @@
+package demoGit;
+
+public class DemoGit {
+
+	public static void main(String[] args) {
+		System.out.println("user1 first commit git hub push");
+		System.out.println("user1 second commit");
+		System.out.println("user1 third commit");
+		System.out.println("user2 first commit ");
+		System.out.println("user1 to create conflict");
+		System.out.println("user2 second commit");
+		System.out.println("new branch to git hub");
+	}
+
+}
